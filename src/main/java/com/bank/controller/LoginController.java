0f7c32eb21eec331d3a2c9ee7836b095ec65b1ba
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-07-24 23:31:23 +0300

    day9

--- src/main/java/com/bank/controller/LoginController.java
+++ src/main/java/com/bank/controller/LoginController.java
@@ -21,15 +21,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/auth")
 public class LoginController {
